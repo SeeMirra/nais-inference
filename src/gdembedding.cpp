bea0d999bb518c7a28083cee2bbb1c8a03a1d446
Christian Mirra <cmirra@gmail.com>
2025-04-16 18:48:21 -0500

    added modelpath

--- src/gdembedding.cpp
+++ src/gdembedding.cpp
@@ -56,6 +56,8 @@ GDEmbedding::GDEmbedding() : params {llama_context_params ()},
 {
     glog_verbose("GDEmbedding constructor");
 
+    std::string model_path; 
+
     struct llama_context_params {
         int n_threads;
         int n_gpu_layers;
